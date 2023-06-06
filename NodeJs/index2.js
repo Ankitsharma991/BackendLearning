e17c69a3423835bf5a7712cf720e05f7b81dc1a1
Ankit Sharma <sharmaa7861@gmail.com>
2023-06-06 22:56:06 +0530

    updating changes

--- NodeJs/index2.js
+++ NodeJs/index2.js
@@ -2,15 +2,21 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+// Using middle wares
 // express.static("./public");
-// app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.static(path.join(path.resolve(), "public")));
 // console.log(path.join(path.resolve(), "public"));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs"); // setting up view engine
 app.get("/", (req, res) => {
   res.render("form", { myName: "Ankit Sharma" });
 });
-
+app.post("/", (req, res) => {
+  console.log(req.body);
+  users.push({ username: req.body.name, email: req.body.email });
+});
 app.listen(5500, () => {
   console.log("Server is working");
 });
