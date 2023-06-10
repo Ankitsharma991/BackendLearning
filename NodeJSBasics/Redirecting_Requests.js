26a9345b7e0e6521b87c0f75a217dbe4d8827a2f
Ankit Sharma <sharmaa7861@gmail.com>
2023-06-11 01:10:21 +0530

    Redirecting Requests

---
+++ NodeJSBasics/Redirecting_Requests.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const method = req.method;
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message' /><button>send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write(
+    "<body><h1>Hello from my NodeJs Server</h1><p>My Name is Ankit Sharma</p></body>"
+  );
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3005);
