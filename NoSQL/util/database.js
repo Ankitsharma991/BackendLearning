cd98287e94cad9c0bcf5dad9813e4c3269570815
Ankit Sharma <sharmaa7861@gmail.com>
2023-07-05 23:59:07 +0530

    updating changes

--- NoSQL/util/database.js
+++ NoSQL/util/database.js
@@ -1,18 +1,29 @@
 const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
+let _db;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://UserName:An_kit.11@cluster0.8o3ect1.mongodb.net/"
+    "mongodb+srv://UserName:An_kit.11@cluster0.8o3ect1.mongodb.net/`"
   )
     .then((client) => {
-      console.log("Connected");
+      console.log("Database Connected Successfully!!");
+      _db = client.db("shop");
       callback(client);
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
 
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!!";
+};
+
 module.exports = mongoConnect;
+exports.getDb = getDb;
