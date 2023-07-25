8dbedf76f4e748c34915e562fb0be1c134a10700
Ankit Sharma <sharmaa7861@gmail.com>
2023-07-25 18:35:45 +0530

    updating changes

---
+++ GraphQL/backend/graphql/schema.js
@@ -0,0 +1,16 @@
+const { buildSchema } = require("graphql");
+
+module.exports = buildSchema(`
+    type TestData {
+        test: String!
+        views: Int!
+    }
+    
+    type RootQuery{
+        hello : String
+    }
+    
+    schema{
+        query: RootQuery
+    }
+`);
